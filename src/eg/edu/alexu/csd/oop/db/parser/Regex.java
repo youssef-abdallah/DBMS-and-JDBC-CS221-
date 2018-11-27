c0203ef52bdaac370e3d9b51cb357af1ba0b268d
DELL <DELL@192.168.1.30>
2018-11-28 01:44:57 +0200

    added insert in regex

--- src/eg/edu/alexu/csd/oop/db/parser/Regex.java
+++ src/eg/edu/alexu/csd/oop/db/parser/Regex.java
@@ -1,5 +1,6 @@
 package eg.edu.alexu.csd.oop.db.parser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -7,8 +8,9 @@ import java.util.regex.Pattern;
 
 public class Regex {
 
-	private static HashMap<String, Object> map = new HashMap<String, Object>();
-	private static HashMap<String, String> colMap = new HashMap<String, String>();
+	private HashMap<String, Object> map = new HashMap<String, Object>();
+	private HashMap<Object, String> colMap = new HashMap<Object, String>();
+	private ArrayList<String> list = new ArrayList<>();
 
 	public boolean validate(String str2check) {
 		String regex = "[a-zA-Z]+\\s+(\\*\\s)?[a-zA-Z,0-9]+\\s+[a-zA-Z <0-9_>='(),]+\\s*";
@@ -18,13 +20,13 @@ public class Regex {
 	}
 
 	private boolean validate(String regex, String str2check) {
-		Pattern pattern = Pattern.compile(regex);
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher m = pattern.matcher(str2check);
 		return m.matches();
 	}
 
 	private String getGroupFromQuery(String regex, String str2check, int num) {
-		Pattern pattern = Pattern.compile(regex);
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher m = pattern.matcher(str2check);
 		if (m.find()) {
 			return m.group(num);
@@ -33,7 +35,7 @@ public class Regex {
 	}
 
 	private void fillColMap(String regex, String str2check) {
-		Pattern pattern = Pattern.compile(regex);
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher m = pattern.matcher(str2check);
 		while (m.find()) {
 			colMap.put(m.group(1), m.group(2));
@@ -41,21 +43,47 @@ public class Regex {
 		map.put("colMap", colMap);
 	}
 
+	private void fillValuesMap(String regex, String str2check, boolean flag) {// if true values if false columns
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher m = pattern.matcher(str2check);
+		if (flag) {
+			if (list.isEmpty()) {
+				int i = 0;
+				while (m.find()) {
+					colMap.put(i, m.group(2));
+					i++;
+				}
+			} else {
+				int i = 0;
+				while (m.find()) {
+					colMap.put(list.get(i), m.group(2));
+					i++;
+				}
+			}
+			map.put("colMap", colMap);
+		} else {
+			while (m.find()) {
+				list.add(m.group(2));
+			}
+		}
+
+	}
+
 	private void parseDelete(String query) {
-		String regex1 = "[a-zA-Z]{6}\\s+[a-zA-Z]{4}\\s+([a-zA-Z0-9_]+)\\s*";
-		String regex2 = regex1.concat("[a-zA-Z]{5}\\s+([a-zA-Z=><'0-9 ]+)\\s*");
+		String regex1 = "delete\\s+from\\s+([a-zA-Z0-9_]+)\\s*";
+		String regex2 = regex1.concat("where\\s+([a-zA-Z=><'0-9 ]+)\\s*");
 		if (validate(regex2, query)) {
 			map.put("tableName", getGroupFromQuery(regex1, query, 1));
 			map.put("where", getGroupFromQuery(regex2, query, 2));
 		} else if (validate(regex1, query)) {
 			map.put("tableName", getGroupFromQuery(regex1, query, 1));
-			map.put("where", "ALL");
+			map.put("where", null);
 		}
 	}
 
 	private void parseUpdate(String query) {
-		String regex1 = "[a-zA-Z]{6}\\s+([a-zA-Z0-9_]+)\\s+[a-zA-Z]{3}\\s+([a-zA-Z0-9,_]+\\s*=\\s*[a-zA-Z0-9,_ ']+\\s)+\\s*";
-		String regex2 = regex1.concat("[a-zA-Z]{5}\\s+([a-zA-Z=><'0-9 ]+)\\s*");
+		String regex1 = "update\\s+([a-zA-Z0-9_]+)\\s+set\\s+([a-zA-Z0-9,_]+\\s*=\\s*[a-zA-Z0-9,_ ']+\\s)+\\s*";
+		String regex2 = regex1.concat("where\\s+([a-zA-Z=><'0-9 ]+)\\s*");
 		if (validate(regex2, query)) {
 			map.put("tableName", getGroupFromQuery(regex1, query, 1));
 			map.put("where", getGroupFromQuery(regex2, query, 3));
@@ -63,14 +91,28 @@ public class Regex {
 			fillColMap("[^WHERE]\\s([a-zA-Z0-9_]+)\\s*=\\s*([a-zA-Z0-9_]+)", query);
 		} else if (validate(regex1, query)) {
 			map.put("tableName", getGroupFromQuery(regex1, query, 1));
-			map.put("where", "ALL");
+			map.put("where", null);
 			fillColMap("[^WHERE]\\s([a-zA-Z0-9_]+)\\s*=\\s*(\\'[a-zA-Z0-9_ ]+\\')", query);
 			fillColMap("[^WHERE]\\s([a-zA-Z0-9_]+)\\s*=\\s*([a-zA-Z0-9_]+)", query);
 		}
 
 	}
 
+	private void parseInsert(String query) {// problem with strings with spaces
+		String regex1 = "insert\\s+into\\s+([a-zA-z0-9_]+)\\s+[(]((\\s*([a-zA-Z0-9_']+)\\s*\\,?)+)[)]\\s*values\\s+[(]((\\s*([a-zA-Z0-9_'.]+)\\s*\\,?)+)[)]\\s*";
+		String regex2 = "insert\\s+into\\s+([a-zA-z0-9_]+)\\s+values\\s+[(]((\\s*([a-zA-Z0-9_'.]+)\\s*\\,?)+)[)]\\s*";
+		if (validate(regex1, query)) {
+			map.put("tableName", getGroupFromQuery(regex1, query, 1));
+			fillValuesMap("(\\s*([a-zA-Z0-9_]+)\\s*\\,?)", getGroupFromQuery(regex1, query, 2), false);
+			fillValuesMap("(\\s*([a-zA-Z0-9_'.]+)\\s*\\,?)", getGroupFromQuery(regex1, query, 5), true);
+		} else if (validate(regex2, query)) {
+			map.put("tableName", getGroupFromQuery(regex2, query, 1));
+			fillValuesMap("(\\s*([a-zA-Z0-9_'.]+)\\s*\\,?)", getGroupFromQuery(regex2, query, 2), true);
+		}
+	}
+
 	public Map<String, Object> parseQuery(String query) {
+		list.clear();
 		String operation = getGroupFromQuery("([a-zA-Z]{6})\\s", query, 1);
 		switch (operation.toUpperCase()) {
 		case ("SELECT"):
@@ -80,6 +122,8 @@ public class Regex {
 			parseUpdate(query);
 			break;
 		case ("INSERT"):
+			map.put("operation", "INSERT");
+			parseInsert(query);
 			break;
 		case ("DELETE"):
 			map.put("operation", "DELETE");
@@ -91,5 +135,4 @@ public class Regex {
 		}
 		return map;
 	}
-
 }
