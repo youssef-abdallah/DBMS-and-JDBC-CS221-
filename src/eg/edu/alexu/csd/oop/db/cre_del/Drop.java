86e628b385317820ed6e2424bf9c4dbc6342807a
DELL <DELL@192.168.1.30>
2018-11-28 18:18:38 +0200

    drop & create classes (missing methods)

---
+++ src/eg/edu/alexu/csd/oop/db/cre_del/Drop.java
@@ -0,0 +1,24 @@
+package eg.edu.alexu.csd.oop.db.cre_del;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Drop {
+
+	public void dropDatabase(File files) throws IOException {
+
+		for (File file : files.listFiles()) {
+			if (file.isDirectory()) {
+				dropDatabase(file);
+			} else {
+				if (!file.delete()) {
+					throw new IOException();
+				}
+			}
+		}
+		if (!files.delete()) {
+			throw new IOException();
+		}
+
+	}
+}
