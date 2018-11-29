86013302289c2d4bdd7615fb86379dca44a470a2
Youssef Abdallah <youssefabdullah@hotmail.com>
2018-11-29 14:34:16 +0200

    interpreting expression in facade

--- src/eg/edu/alexu/csd/oop/db/facade/Facade.java
+++ src/eg/edu/alexu/csd/oop/db/facade/Facade.java
@@ -7,8 +7,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import eg.edu.alexu.csd.oop.db.cre_del.Create;
+import eg.edu.alexu.csd.oop.db.expressions.Context;
 import eg.edu.alexu.csd.oop.db.expressions.Expression;
 import eg.edu.alexu.csd.oop.db.expressions.ExpressionsFactory;
 import eg.edu.alexu.csd.oop.db.files.DTD;
@@ -21,21 +23,29 @@ public class Facade {
 	private boolean dropIfExists;
 	private HashMap<String, Object> map;
 	private Expression exp;
+	private ExpressionsFactory factory;
+	private Context ctx;
+	private Object[][] result;
+
+	public Object[][] getResult() {
+		return result;
+	}
 
 	public Facade(String query, boolean drop) {
 		parser = new Regex();
 		this.query = query;
 		dropIfExists = drop;
+		factory = new ExpressionsFactory();
 	}
 
 	public void evaluateQuery() throws SQLException {
 		map = parser.parseQuery(query);
-		String tableName = (String)map.get("tableName");
-		String condition = (String)map.get("where");
-		HashMap <String, String> colVal = (HashMap)map.get("colMap");
 		if (map.isEmpty()) {
 			throw new java.sql.SQLException();
 		}
+		String tableName = (String)map.get("tableName");
+		String condition = (String)map.get("where");
+		HashMap <String, String> colVal = (HashMap)map.get("colMap");
 		String operationName = (String) map.get("operation");
 		if (operationName.equalsIgnoreCase("create database")) {
 			boolean checkDir = false;
@@ -57,8 +67,24 @@ public class Facade {
 			HashMap<String, String> colMap = (HashMap<String, String>) map.get("colMap");
 			dtdFile.Write(currentDatabase, (String) map.get("tableName"),
 					new ArrayList(Arrays.asList(colMap.keySet().toArray())));
-		} else if (operationName.equalsIgnoreCase("insert")) {
-		   exp = new ExpressionsFactory("insert", tableName, condition, colVal);
+		} else {
+		   exp = factory.makeExpression(operationName, tableName, condition, colVal);
+		   DTD dtd = new DTD();
+		   String path = System.getProperty("file.separator") + "Databases" + System.getProperty("file.separator")
+			+ map.get("databaseName");
+		   List<String> schema = dtd.read(path, tableName);
+		   ctx = new Context(schema);
+		   List<String> interpretation = exp.interpret(ctx);
+		   String[] s = interpretation.get(0).split(" ");
+	        int rows = interpretation.size();
+	        int cols = s.length;
+	        result = new Object[rows][cols];
+	        for(int i = 0; i < rows; i++) {
+	        	String[] tmp = interpretation.get(i).split(" ");
+	        	for(int j = 0; j < cols; j++) {
+	        		result[i][j] = tmp[j];
+	        	}
+	        }
 		}
 
 	}
