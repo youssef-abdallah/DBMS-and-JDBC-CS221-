50d3906d93352e880eb0ce0ec32877833f458ccd
DELL <DELL@192.168.1.30>
2018-11-29 16:28:23 +0200

    .

--- src/eg/edu/alexu/csd/oop/db/parser/Regex.java
+++ src/eg/edu/alexu/csd/oop/db/parser/Regex.java
@@ -131,7 +131,7 @@ public class Regex {
 	}
 
 	private void parseCreate(String query) {
-		String regex1 = "\\s*create\\s+database\\s+([a-zA-Z0-9_\\/]+)\\s*";
+		String regex1 = "\\s*CREATE\\s+DATABASE\\s+([a-zA-Z0-9_\\\\/]+)\\s*";
 		String regex2 = "\\s*create\\s+table\\s+([a-zA-Z_0-9]+)\\s*[(]((\\s*([a-zA-Z_0-9]+)\\s+(varchar||int)\\s*\\,?)+)[)]\\s*";
 		if (validate(regex1, query.trim())) {
 			map.put("operation", "CREATE DATABASE");
